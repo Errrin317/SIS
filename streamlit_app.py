80495c5847f49be216453b51834bd535f832408f
Errrin317 <64836128+Errrin317@users.noreply.github.com>
2024-08-14 20:20:14 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,8 @@ if ingrediant_list:
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('search_on')
+        st.write(search_on)
         
         st.subheader(i + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "search_on")
